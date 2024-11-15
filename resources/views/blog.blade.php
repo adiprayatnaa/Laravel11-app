f1fdc54bcf9795943876910d23cbb4fcc0d95770
adiprayatnaa <gilaradipray@gmail.com>
2024-11-15 14:31:56 +0700

    menambahkan tailwindui

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h5>Intelijen</h5>
+    <p>Intelijen adalah bidang yang melibatkan pengumpulan, analisis, dan penggunaan informasi yang relevan untuk mendukung pengambilan keputusan, terutama dalam konteks keamanan nasional, politik, dan militer. Intelijen dapat mencakup berbagai kegiatan mulai dari pengintaian hingga analisis data strategis. Berikut beberapa poin penting tentang dunia intelijen <br></p>
+
+    <h5>Kriptografi</h5>
+    <p>Kriptografi adalah cabang ilmu yang mempelajari teknik untuk mengamankan informasi agar hanya dapat diakses oleh pihak yang berwenang. Kriptografi telah digunakan sejak zaman kuno, tetapi semakin berkembang pesat seiring kemajuan teknologi digital <br></p>
+
+
+</body>
+</html>
